af511f7cdc4663f28b08580b2cfb61e22fc9e068
210410196 <210410196@gms.tku.edu.tw>
2023-02-15 19:59:56 +0800

    W01-P1: upload 4 phots to supabase

---
+++ src/data-0.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img:
+      'https://hzllwkixijuoqbropnat.supabase.co/storage/v1/object/public/demo-96/cart-96/galaxy-s7.png',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img:
+      'https://hzllwkixijuoqbropnat.supabase.co/storage/v1/object/public/demo-96/cart-96/google-pixel.png',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img:
+      'https://hzllwkixijuoqbropnat.supabase.co/storage/v1/object/public/demo-96/cart-96/redmi-notes.png',
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: 'Samsung Galaxy S8',
+    price: 399.99,
+    img:
+      'https://hzllwkixijuoqbropnat.supabase.co/storage/v1/object/public/demo-96/cart-96/galaxy-s8.png',
+    amount: 1,
+  },
+];
