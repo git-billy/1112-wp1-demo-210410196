5c005b6aec890d6d4bbc2d6863814268ef05a861
Billy <billy@pc99.wireless10.tku.edu.tw>
2023-04-13 21:33:10 +0800

    [middle-exam]: P2.5

--- middle-exam/midprep_client_96/src/assets/wrapper/midprep_96/Products_scss_96.js
+++ middle-exam/midprep_client_96/src/assets/wrapper/midprep_96/Products_scss_96.js
@@ -1,39 +1,215 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  .collection-overview {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-  }
+    .blogs {
+    --primary: #645cff;
+    --primary-dark: #3c3799;
+    --grey-light: #f1f5f9;
+    --grey: #64748b;
+    --grey-dark: #0f172a;
+    --letter-spacing: 2px;
+    --fluid-width: 90vw;
+    --max-width: 1170px;
+    --border-radius: 0.15rem;
+    padding: 5rem 0;
 
-  .collection-preview {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    margin-bottom: 30px;
-  }
+      .section-title {
+          text-align: center;
+          text-transform: capitalize;
+          margin: 2rem;
+          letter-spacing: 2px;
+      }
 
-  .collection-preview .title {
-    font-size: 20px;
-    margin-bottom: 25px;
-  }
+      .blogs-center {
+          display: grid;
+          grid-template-columns: 1fr;
+          gap: 2rem;
+          width: var(--fluid-width);
+          max-width: var(--max-width);
+          margin: 0 auto;
+          /* max-width: 500px; */
+      }
 
-  .collection-preview .preview {
-    display: -ms-grid;
-    display: grid;
-    -ms-grid-columns: (1fr) [4];
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 10px;
-  }
-`;
+      @media screen and (min-width: 450px) {
+          .blogs-center {
+          grid-template-columns: repeat(2, 1fr);
+          }
+      }
+
+      @media screen and (min-width: 768px) {
+          .blogs-center {
+          grid-template-columns: repeat(3, 1fr);
+          }
+      }
+
+      @media screen and (min-width: 992px) {
+          .blogs-center {
+          grid-template-columns: repeat(4, 1fr);
+          }
+      }
+
+      .blogs-center2 {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(13rem, 1fr));
+          gap: 2rem;
+          width: var(--fluid-width);
+          max-width: var(--max-width);
+          margin: 0 auto;
+          /* max-width: 500px; */
+      }
+
+      .blog {
+          background-color: white;
+          padding: 0.75rem;
+          border-radius: var(--border-radius);
+          margin-bottom: 2rem;
+          box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+          transition: 0.5s ease-in-out all;
+
+          &:hover {
+          box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+          }
+
+          .img {
+          width: 100%;
+          height: 12rem;
+          display: block;
+          object-fit: cover;
+          }
+
+          &-img {
+          border-radius: var(--border-radius);
+          margin-bottom: 0.5rem;
+          }
+
+          &-content {
+          padding-bottom: 1rem;
+          }
+
+          .blog-content span {
+          text-transform: uppercase;
+          color: var(--primary);
+          letter-spacing: var(--letter-spacing);
+          font-weight: 500;
+          }
+
+          .blog-content h3 {
+          text-transform: capitalize;
+          letter-spacing: var(--letter-spacing);
+          margin: 0.5rem 0;
+          }
+
+          .blog-content p {
+          color: var(--grey);
+          margin-bottom: 0.5rem;
+          }
+
+          .blog-content a {
+          text-transform: capitalize;
+          color: var(--primary-dark);
+          letter-spacing: 1px;
+          }
+      }
+    }
+
+    .collection-page {
+      display: flex;
+      flex-direction: column;
+    
+      .title {
+        font-size: 38px;
+        margin: 15px auto;
+      }
+    
+      .items {
+        // display: flex;
+        // justify-content: flex-start;
+        // flex-wrap: wrap;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 10px;
+      }
+    }
+    
+    .collection-item {
+      width: 22vw;
+      display: flex;
+      flex-direction: column;
+      height: 300px;
+      align-items: center;
+      position: relative;
+      margin-bottom: 25px;
+    
+      .image {
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center center;
+        margin-bottom: 5px;
+      }
+    
+      .custom-button {
+        width: 80%;
+        position: absolute;
+        top: 205px;
+        display: none;
+        opacity: 0.7;
+        padding: 10px 10px;
+        font-size: 20px;
+        font-weight: bold;
+    
+        &:hover {
+          background-color: #000;
+          color: white;
+        }
+      }
+    
+      &:hover {
+        .image {
+          opacity: 0.8;
+        }
+    
+        .custom-button {
+          opacity: 0.85;
+          display: block;
+        }
+      }
+    }
+    
+    .collection-footer {
+      width: 100%;
+      height: 5%;
+      display: flex;
+      justify-content: space-between;
+      font-size: 14px;
+    }
+    
+    // Overview & Preview
+    .collection-overview {
+      display: flex;
+      flex-direction: column;
+    }
+    
+    .collection-preview {
+      display: flex;
+      flex-direction: column;
+      margin: auto auto 30px auto;
+    
+      .title {
+        text-align: start;
+        font-size: 20px;
+        margin-bottom: 25px;
+      }
+    
+      .preview {
+        // display: flex;
+        // justify-content: space-around;
+        // flex-wrap: wrap;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 10px;
+      }
+    }
+`
 
 export default Wrapper;
--- middle-exam/midprep_client_96/src/pages/midprep_96/ProductsStaticPage_96.js
+++ middle-exam/midprep_client_96/src/pages/midprep_96/ProductsStaticPage_96.js
@@ -1,88 +1,125 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Wrapper from '../../assets/wrapper/midprep_96/Products_scss_96';
+import Wrapper from "../../assets/wrapper/midprep_96/Products_scss_96";
 
 // import Wrapper from '../../assets/wrapper/midprep_xx/Products_css_xx';
 
 const ProductsStaticPage_96 = () => {
-  const [name, setName] = useState('ChungChun Wang');
-  const [id, setId] = useState('210410196');
+  const [name, setName] = useState("ChungChun Wang");
+  const [id, setId] = useState("210410196");
 
   return (
     <Wrapper>
-      <div class='shop-page'>
-        {/* <h1 style='text-align: center'>
-          Midprep_96:{name} {id}
-        </h1> */}
-        <div class='collection-overview'>
-          <div class='collection-preview'>
-            <h2 class='title'>Hats</h2>
-            <div class='preview'>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Brown Brim</span>
-                  <span class='price'>25</span>
+      <section className="blogs">
+        <div className="section-title">
+          <h2>Fetch Blogs From Local Json Data</h2>
+          <h3>
+            {name} {id}
+          </h3>
+        </div>
+        <div className="shop-page">
+          <div className="collection-overview">
+            <div className="collection-preview">
+              <h2 className="title">Hats</h2>
+              <div className="preview">
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/hats/brown-brim.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Brown Brim</span>
+                    <span className="price">25</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/blue-beanie.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Blue Beanie</span>
-                  <span class='price'>18</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/hats/blue-beanie.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Blue Beanie</span>
+                    <span className="price">18</span>
+                  </div>
+                  <button className="custom-button">Add to Card</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-cowboy.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Brown Cowboy</span>
-                  <span class='price'>35</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/hats/brown-cowboy.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Brown Cowboy</span>
+                    <span className="price">35</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/grey-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Grey Brim</span>
-                  <span class='price'>25</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/hats/grey-brim.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Grey Brim</span>
+                    <span className="price">25</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
               </div>
             </div>
-          </div>
-          <div class='collection-preview'>
-            <h2 class='title'>Womens</h2>
-            <div class='preview'>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Blue Tanktop</span>
-                  <span class='price'>25</span>
+            <div className="collection-preview">
+              <h1 className="title">Jackets</h1>
+              <div className="preview">
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/jackets/black-shearling.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Black Jean Shearling</span>
+                    <span className="price">125</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Floral Blouse</span>
-                  <span class='price'>20</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/jackets/blue-jean-jacket.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Blue Jean Jacket</span>
+                    <span className="price">90</span>
+                  </div>
+                  <button className="custom-button">Add to Card</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Floral Dress</span>
-                  <span class='price'>80</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/jackets/grey-jean-jacket.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Grey Jean Jacket</span>
+                    <span className="price">90</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
-              </div>
-              <div class='collection-item'>
-                <img class='image' src='/crown/img/hats/brown-brim.png' />
-                <div class='collection-footer'>
-                  <span class='name'>Red Dots Dress</span>
-                  <span class='price'>80</span>
+                <div className="collection-item">
+                  <img
+                    className="image"
+                    src="/crown/img/jackets/brown-shearling.png"
+                  />
+                  <div className="collection-footer">
+                    <span className="name">Brown Shearling</span>
+                    <span className="price">165</span>
+                  </div>
+                  <button className="custom-button">Add to Cart</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </Wrapper>
   );
 };
