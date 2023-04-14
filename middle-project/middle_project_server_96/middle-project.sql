438bc7605240c57b906107da8d4ca4f52944ae80
Billy <billy@BillydeMacBook-Pro.local>
2023-04-14 09:28:28 +0800

    [middle-project]: add middle-project.sql

--- middle-project/middle_project_server_96/middle-project.sql
+++ middle-project/middle_project_server_96/middle-project.sql
@@ -1,5 +1,4 @@
-
-DROP TABLE middle_project_96
+DROP TABLE middle_product_96
 
 CREATE TABLE middle_product_96 (
     pId int,
@@ -11,11 +10,11 @@ CREATE TABLE middle_product_96 (
 
 INSERT INTO middle_product_96
 VALUES
-(1, "The Taiwan", 100, '5', "https://flagcdn.com/w320/tw.png"),
-(2, "The United States", 200, '5', "https://flagcdn.com/w320/us.png"),
-(3, "The Germany", 300, '5', "https://flagcdn.com/w320/de.png"),
-(4, "The Australia", 400, '5', "https://flagcdn.com/w320/au.png"),
-(5, "The Austria", 500, '5', "https://flagcdn.com/w320/at.png"),
-(6, "The Česko", 600, '5', "https://flagcdn.com/w320/cz.png"),
-(7, "The Swiss", 700, '5', "https://flagcdn.com/w320/ch.png"),
-(8, "The Taiwan", 800, '5', "https://flagcdn.com/w320/ax.png"),
+(1, 'The Taiwan', 100, '5', 'https://flagcdn.com/w320/tw.png'),
+(2, 'The United States', 200, '5', 'https://flagcdn.com/w320/us.png'),
+(3, 'The Germany', 300, '5', 'https://flagcdn.com/w320/de.png'),
+(4, 'The Australia', 400, '5', 'https://flagcdn.com/w320/au.png'),
+(5, 'The Austria', 500, '1', 'https://flagcdn.com/w320/at.png'),
+(6, 'The Česko', 600, '1', 'https://flagcdn.com/w320/cz.png'),
+(7, 'The Swiss', 700, '1', 'https://flagcdn.com/w320/ch.png'),
+(8, 'The Taiwan', 800, '1', 'https://flagcdn.com/w320/ax.png')
